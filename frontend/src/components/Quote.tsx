c36b77ce4483b8646b3b9fd9e0f1a9348f3dee1b
Nevil844 <nevil.j@ahduni.edu.in>
2024-04-22 21:01:25 +0530

    first commit

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    Welcome to a Blogging Website similar to Medium.
+                </div>
+                <div className="max-w-md text-xl font-semibold text-left mt-4">
+                    Nevil Jobanputra
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                    Developer
+                </div>
+            </div>
+        </div>
+        
+    </div>
+}
